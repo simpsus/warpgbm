cd34ec8d1312e9ba86a0d356f93b0d913cf9d38d
Bastian Grumbrecht <bastian.kennel@gmail.com>
2025-06-30 22:22:53 +0200

    putting metric calculation into the metrics module

--- warpgbm/metrics.py
+++ warpgbm/metrics.py
@@ -8,3 +8,13 @@ def rmsle_torch(y_true, y_pred, eps=1e-7):
     log_true = torch.log1p(y_true + eps)
     log_pred = torch.log1p(y_pred + eps)
     return torch.sqrt(torch.mean((log_true - log_pred) ** 2))
+
+def get_eval_metric(metric, y_true, y_pred):
+    if metric == "mse":
+        return ((y_true - y_pred) ** 2).mean().item()
+    elif metric == "corr":
+        return 1 - torch.corrcoef(torch.vstack([y_true, y_pred]))[0, 1].item()
+    elif metric == "rmsle":
+        return rmsle_torch(y_true, y_pred).item()
+    else:
+        raise ValueError(f"Invalid eval_metric: {metric}.")
